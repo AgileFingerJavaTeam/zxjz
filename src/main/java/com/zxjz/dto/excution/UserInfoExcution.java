d84e52a8955cf3566ab3f60b9ad901193b0b9913
sunxiaoyuan <sun3188360@163.com>
2017-09-22 18:18:49 +0800

    修改项目结构

--- src/main/java/com/zxjz/dto/excution/UserInfoExcution.java
+++ src/main/java/com/zxjz/dto/excution/UserInfoExcution.java
@@ -1,23 +1,28 @@
 package com.zxjz.dto.excution;
 
-import com.zxjz.base.BaseExcution;
 import com.zxjz.entity.UserInfo;
 import com.zxjz.enums.UserInfoEnum;
 
 /**
  * Created by sunzhongyuan on 2017/9/20.
  */
-public class UserInfoExcution extends BaseExcution{
+public class UserInfoExcution {
 
     private UserInfo userInfo;
 
-    public UserInfoExcution(UserInfoEnum userInfoEnum, UserInfo userInfo) {
-        super(userInfoEnum);
-        this.userInfo = userInfo;
-    }
+    private int code;
+
+    private String codeInfo;
 
     public UserInfoExcution(UserInfoEnum userInfoEnum) {
-        super(userInfoEnum);
+        this.code = userInfoEnum.getCode();
+        this.codeInfo = userInfoEnum.getCodeInfo();
+    }
+
+    public UserInfoExcution(UserInfoEnum userInfoEnum,UserInfo userInfo) {
+        this.code = userInfoEnum.getCode();
+        this.codeInfo = userInfoEnum.getCodeInfo();
+        this.userInfo = userInfo;
     }
 
     public UserInfo getUserInfo() {
@@ -28,10 +33,28 @@ public class UserInfoExcution extends BaseExcution{
         this.userInfo = userInfo;
     }
 
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getCodeInfo() {
+        return codeInfo;
+    }
+
+    public void setCodeInfo(String codeInfo) {
+        this.codeInfo = codeInfo;
+    }
+
     @Override
     public String toString() {
         return "UserInfoExcution{" +
                 "userInfo=" + userInfo +
+                ", code=" + code +
+                ", codeInfo='" + codeInfo + '\'' +
                 '}';
     }
 }
