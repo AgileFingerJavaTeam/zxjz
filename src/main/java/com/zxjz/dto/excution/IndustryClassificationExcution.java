5c0937ee3ada6b40ef1e50af76a5dcc060a39ff2
illusionGy <2498873793@qq.com>
2017-10-14 15:48:30 +0800

    修改IndustruClass文件

--- src/main/java/com/zxjz/dto/excution/IndustryClassificationExcution.java
+++ src/main/java/com/zxjz/dto/excution/IndustryClassificationExcution.java
@@ -1,56 +1,16 @@
 package com.zxjz.dto.excution;
 
+import com.zxjz.base.BaseAPIExcution;
 import com.zxjz.entity.IndustryClassification;
 import com.zxjz.enums.CityEnum;
 
 import java.util.List;
 
-public class IndustryClassificationExcution {
-    private List<IndustryClassification> listIndustryClassification;
-    private int code;
-    private String codeInfo;
+public class IndustryClassificationExcution extends BaseAPIExcution{
 
-    public IndustryClassificationExcution(List<IndustryClassification> listIndustryClassification, CityEnum cityEnum) {
-        this.listIndustryClassification = listIndustryClassification;
+    public IndustryClassificationExcution(Object data, CityEnum cityEnum) {
+        this.data = data;
         this.code = cityEnum.getCode();
-        this.codeInfo = cityEnum.getCodeInfo();
     }
 
-    public IndustryClassificationExcution(CityEnum cityEnum) {
-        this.code = cityEnum.getCode();
-        this.codeInfo = cityEnum.getCodeInfo();
-    }
-
-    public List<IndustryClassification> getListIndustryClassification() {
-        return listIndustryClassification;
-    }
-
-    public void setListIndustryClassification(List<IndustryClassification> listIndustryClassification) {
-        this.listIndustryClassification = listIndustryClassification;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public void setCode(int code) {
-        this.code = code;
-    }
-
-    public String getCodeInfo() {
-        return codeInfo;
-    }
-
-    public void setCodeInfo(String codeInfo) {
-        this.codeInfo = codeInfo;
-    }
-
-    @Override
-    public String toString() {
-        return "IndustryClassificationExcution{" +
-                "listIndustryClassification=" + listIndustryClassification +
-                ", code=" + code +
-                ", codeInfo='" + codeInfo + '\'' +
-                '}';
-    }
 }
